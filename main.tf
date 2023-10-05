868cad977dbb67f8e2a811b1bbd41b1188fb3595
punithnayini <147057558+punithnayini@users.noreply.github.com>
2023-10-05 18:36:17 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,87 @@
+resource "azurerm_resource_group" "example" {
+  name     = "vmrg"
+  location = "East us"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "vm-network"
+  address_space       = ["12.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["12.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "vm0-ip"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "vm-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.example.id
+  }
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "vmnsg"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_network_security_rule" "example" {
+  name                        = "rdp"
+  priority                    = 100
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "3389"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.example.name
+  network_security_group_name = azurerm_network_security_group.example.name
+}
+
+resource "azurerm_network_interface_security_group_association" "example" {
+    network_interface_id      = azurerm_network_interface.example.id
+    network_security_group_id = azurerm_network_security_group.example.id
+}
+
+resource "azurerm_windows_virtual_machine" "example" {
+  name                = "tfvm"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_D2s_v3"
+  admin_username      = "tfvm"
+  admin_password      = "Puneeth8790@"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
---
+++ provider.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  features {}
+  version ="3.73.0"
+  client_id       = "4a38be90-b4a6-4cbb-9d46-3979b40562f9"
+  client_secret   = "XJ18Q~cN_jVp2lo_fWg1T5aebMpYuR9dvxeF6aAD"
+  tenant_id       = "0fc03882-3292-48db-bd44-0ec3cf434f99"
+  subscription_id = "d8f502fd-891c-463f-b61f-a906ca9ec305"
+}
